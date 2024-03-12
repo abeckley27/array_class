92c8f4c683e44c8d307104a02192c65c5addaa32
abeckley27 <aidanbeckley@gmail.com>
2024-03-11 21:08:52 -0400

    Added addition, subtraction, and dot product

--- array.h
+++ array.h
@@ -7,12 +7,13 @@
 template <typename T>
 class array {
 public:
-    // Default Constructor: Initializes all elements to a default value (e.g., 0 for int, nullptr for pointers).
+    // Default Constructor: Initializes data to a null pointer. Useful only for reassignment
     array();
 
     //Other constructors
-    array(int n, T);           //Initialize by setting the entire array to a certain value
-    array(const array<T>&);      //copy constructor
+    array(int n, T);            //Initialize by setting the entire array to a certain value
+    array(const array<T>&);     //copy constructor
+    array(T*, int len_);        //Initialize using data is a raw array
 
     // Destructor
     ~array() { this->clear(); }
@@ -20,15 +21,22 @@ public:
     // Accessor methods:
     size_t size() const; // Returns the size of the array
     size_t data_size() const;   // Returns the size of the elements, T
-    T& operator[](int i); // Access element at the specified index
+    
     void print();
+    T& operator[](int i); // Access element at the specified index
+
+    //Arithmetic methods
+    void operator+=(const array<T>& a1);
+    void operator-=(const array<T>& a1);
+
+    void add_arrays(const array<T>& a2, const array<T>& a3);
+    void subtract_arrays(const array<T>& a2, const array<T> a3);
+
+    T operator*(const array<T>& a1);   //dot product
     
 
-    //Modifier methods
-    
-    
-
-    //
+    //comparison
+    bool operator==(const array<T>& a1);
     
 
 private:
@@ -70,6 +78,16 @@ inline array<T>::array(const array<T>& a1)
 }
 
 template<typename T>
+inline array<T>::array(T* a1, int len_)
+{
+    T_size = sizeof(*a1);
+    n_alloc = len_;
+    data = new T[len_];
+
+    for (int i = 0; i < len_; i++) { data[i] = a1[i]; }
+}
+
+template<typename T>
 inline void array<T>::clear()
 {
     // Clean up any resources (if applicable)
@@ -84,6 +102,91 @@ template<typename T>
 T& array<T>::operator[](int i)
 {
     return data[i];
+}
+
+template<typename T>
+inline void array<T>::operator+=(const array& a1)
+{
+    if (n_alloc != a1.n_alloc)
+    {
+        //I dunno, don't add them I guess.
+    }
+    else {
+        for (int i = 0; i < n_alloc; i++) { data[i] = data[i] + a1.data[i]; }
+    }
+}
+
+template<typename T>
+inline void array<T>::operator-=(const array& a1)
+{
+    if (n_alloc != a1.n_alloc)
+    {
+        //
+    }
+    else {
+        for (int i = 0; i < n_alloc; i++) { data[i] = data[i] - a1.data[i]; }
+    }
+}
+
+
+template<typename T>
+inline void array<T>::add_arrays(const array<T>& a2, const array<T>& a3)
+{
+    if (this->data != NULL)
+    {
+        for (int i = 0; i < this->n_alloc; i++) { this->data[i] = a2.data[i] + a3.data[i]; }
+    }
+    else {
+        this->n_alloc = a2.n_alloc;
+        if (a3.n_alloc > a2.n_alloc) { this->n_alloc = a3.n_alloc; }
+
+        this->T_size = sizeof(T);
+        this->data = new T[n_alloc];
+
+        // THIS IS SUPER UNSAFE MEMORY ACCESS IF a2 AND a3 HAVE DIFFERENT LENGTHS
+        for (int i = 0; i < this->n_alloc; i++) { this->data[i] = a2.data[i] + a3.data[i]; }
+    }
+}
+
+template<typename T>
+inline void array<T>::subtract_arrays(const array<T>& a2, const array<T> a3)
+{
+    if (this->data != NULL)
+    {
+        for (int i = 0; i < this->n_alloc; i++) { this->data[i] = a2.data[i] - a3.data[i]; }
+    }
+    else {
+        this->n_alloc = a2.n_alloc;
+        if (a3.n_alloc > a2.n_alloc) { this->n_alloc = a3.n_alloc; }
+
+        this->T_size = sizeof(T);
+        this->data = new T[n_alloc];
+
+        // THIS IS SUPER UNSAFE MEMORY ACCESS IF a2 AND a3 HAVE DIFFERENT LENGTHS
+        for (int i = 0; i < this->n_alloc; i++) { this->data[i] = a2.data[i] - a3.data[i]; }
+    }
+}
+
+template<typename T>
+inline T array<T>::operator*(const array& a1)
+{
+    T output = T(1);
+    for (int i = 0; i < n_alloc; i++) { output *= (data[i] * a1.data[i]); }
+
+    return output;
+}
+
+template<typename T>
+inline bool array<T>::operator==(const array<T>& a1)
+{
+    bool output = true;
+    for (int i = 0; i < n_alloc; i++) {
+        if (data[i] != a1.data[i]) {
+            output = false;
+            break;
+        }
+    }
+    return output;
 }
 
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstddef>
 #include <cstdint>
+#include <random>
 
 #include "array.h"
 
@@ -8,14 +9,46 @@
 int main(int argc, char* argv[])
 {
 	array<int> lst1(8, 3);
-	array<double> lst3(10, 3.14159);
+	array<double> lst3(15, 3.14159);
 	array<float> lst2(1000, 0);
 	lst1[0] = 12;
 	lst1[1] = 34;
 	lst3[3] = 33;
 	lst1.print();
 
-	std::cout << lst1.data_size() << '\t' << lst2.data_size() << '\t' << lst3.data_size() << '\n';
+	std::cout << lst1.data_size() << '\t' << lst2.data_size() << '\t' << sizeof(long int) << '\n';
+	double raw_array[10] = { 0.18954932420590787, 0.9815853266153907, 0.1182619429237155, 0.24020743046802462,
+		0.17391436652827874, 0.452261916487374, 0.35184099296650473, 0.42676874830607936,
+		0.6457640043603359,0.5178816338229062};
+	
+	array<double> x(raw_array, 10);
+	x.print();
+
+
+
+	// Create a random number generator engine (using Mersenne Twister in this case)
+	std::mt19937 rng(std::random_device{}());
+
+	// Create a uniform distribution for floating-point numbers between 0 and 1
+	std::uniform_real_distribution<double> dist(0.0, 1.0);
+
+	const int64_t N = 100;
+	array<double> a(N, 0);
+	array<double> b(N, 0);
+
+
+	for (int i = 0; i < N; i++)
+	{
+		a[i] = dist(rng);
+		b[i] = dist(rng);
+	}
+
+	std::cout << "\n\n\n";
+
+	array<double> c;
+	c.add_arrays(a, b);
+	c.print();
+
 
 
 	return 0;
